3853a3499e83f9b6df318f71c13a8807bbbbb616
Pedro290898 <97262451+Pedro290898@users.noreply.github.com>
2026-09-24 00:00:37 -0600

    conexionconinvetariov1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -115,8 +115,8 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 export class AppComponent implements OnInit {
   // Conexión real a tu base de datos
   supabase: SupabaseClient = createClient(
-    'https://supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjMzNDMsImV4cCI6MjAyNjgxOTM0M30.2RDBqneYitXbIOnuA1bcljE2Z8XoU7YitXbIOnuA1bcljE2Z8XoU7Y' // <-- REVISA QUE ESTA SEA TU CLAVE COMPLETA DE SUPABASE
+    'https://srvytgtkcasuylosgzej.supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwOTMxNzAsImV4cCI6MjEwNDY2OTE3MH0.2RD6qneBwTq6IH4QixBOTpJLq9of8BEFKcqicEeyGp4' // <-- REVISA QUE ESTA SEA TU CLAVE COMPLETA DE SUPABASE
   );
 
   user: any = null;
