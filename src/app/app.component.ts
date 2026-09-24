b648bdcad7011666c26dfe66e29ffee3110847d3
Pedro290898 <97262451+Pedro290898@users.noreply.github.com>
2026-09-24 11:37:22 -0600

    modulos3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -51,8 +51,8 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 })
 export class AppComponent implements OnInit {
   supabase: SupabaseClient = createClient(
-    'https://supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTEyMjMzNDMsImV4cCI6MjAyNjgxOTM0M30.2RDBqneYitXbIOnuA1bcljE2Z8XoU7YitXbIOnuA1bcljE2Z8XoU7Y' // <-- REVISA TU CLAVE DE SUPABASE
+    'https:/srvytgtkcasuylosgzej/supabase.co',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwOTMxNzAsImV4cCI6MjEwNDY2OTE3MH0.2RD6qneBwTq6IH4QixBOTpJLq9of8BEFKcqicEeyGp4' // <-- REVISA TU CLAVE DE SUPABASE
   );
 
   user: any = null;
