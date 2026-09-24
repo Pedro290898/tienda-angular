39173a9112f1d2a3eec4df8d82cd4ff9c4200eba
Pedro290898 <97262451+Pedro290898@users.noreply.github.com>
2026-09-23 22:55:31 -0600

    otro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -57,10 +57,15 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 })
 export class AppComponent implements OnInit {
   // ⚠️ COLOCA TU URL Y TU CLAVE LEGACY ANON CORRECTAS DE SUPABASE
-  supabase: SupabaseClient = createClient(
-    'https://srvytgtkcasuylosgzej',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwOTMxNzAsImV4cCI6MjEwNDY2OTE3MH0.2RD6qneBwTq6IH4QixBOTpJLq9of8BEFKcqicEeyGp4'
+    supabase = createClient(
+    'https://supabase.co',
+    'srvytgtkcasuylosgzej'
   );
+
+  //supabase: SupabaseClient = createClient(
+   // 'https://srvytgtkcasuylosgzej',
+   // 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNydnl0Z3RrY2FzdXlsb3NnemVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwOTMxNzAsImV4cCI6MjEwNDY2OTE3MH0.2RD6qneBwTq6IH4QixBOTpJLq9of8BEFKcqicEeyGp4'
+  //);
 
   user: any = null;
   esRegistro = false;
